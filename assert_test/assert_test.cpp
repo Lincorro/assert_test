67ed2b101aea3acf423170c65245ac97715a006f
valmar98@mail.ru <valmar98@mail.ru>
2025-06-23 19:28:49 +0300

    Добавьте файлы проекта.

---
+++ assert_test/assert_test.cpp
@@ -0,0 +1,25 @@
+﻿// assert_test.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cassert>
+
+float travelTime(float distance, float speed) {
+
+    assert(speed > 0 && "Speed must be positive");
+
+    return distance / speed;
+}
+
+int main()
+{
+    float speed = 0;
+    float distance = 0;
+    std::cout << "Input Speed and distance \n";
+    std::cout << "Speed: ";
+    std::cin >> speed;
+    std::cout << "\nDistance: ";
+    std::cin >> distance;
+
+    std::cout << "Time = " << travelTime (distance, speed) ;
+}
